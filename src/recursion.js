9411389d5a59eae9738c11e4b260927a34b64bcf
Jacob Ward <smilinjake@gmail.com>
2021-04-12 22:45:38 -0500

    Implimented up to problem 11

--- src/recursion.js
+++ src/recursion.js
@@ -7,31 +7,97 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  if (n < 0) {
+    return null;
+  } else if (n === 0) {
+    return 1;
+  } else {
+    return (n * factorial(n - 1));
+  }
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  var copy = array.slice(0);
+  if (copy.length === 0) {
+    return 0;
+  } else if (copy.length === 1) {
+    return copy[0];
+  } else {
+    return copy.shift() + sum(copy);
+  }
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+
+  var sum  = 0;
+
+  for (var i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+       sum += arraySum(array[i]);
+    } else {
+       sum += array[i];
+    }
+  }
+
+  return sum;
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  a = Math.abs(n);
+  if (a === 0) {
+    return true;
+  } else if ((a - 2) === 0) {
+    return true;
+  } else if ((a - 2) < 0) {
+    return false;
+  } else {
+    return isEven(a - 2);
+  }
 };
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
-};
+  var isNegative = false;
+  if (n < 0) {
+    isNegative = true;
+  }
+  n = Math.abs(n);
+  if (n === 0 || n === 1) {
+    return 0;
+  } else if (isNegative) {
+    return ((n - 1) + sumBelow(n - 1)) * -1;
+  } else {
+    return ((n - 1) + sumBelow(n - 1));
+  }
+}
 
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+  if (y < x) {
+    if ((x - y) < 2) {
+      return [];
+    } else {
+      var resultArr = range(x, (y + 1));
+      resultArr.push((y + 1));
+      return resultArr;
+    }
+  } else {
+    if ((y - x) < 2) {
+      return [];
+    } else {
+      var resultArr = range(x, (y - 1));
+      resultArr.push((y - 1));
+      return resultArr;
+    }
+  }
 };
 
 // 7. Compute the exponent of a number.
@@ -40,6 +106,15 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+    if (exp === 0) {
+      return 1;
+    } else {
+      if (exp < 0) {
+        return 1 / (base * exponent(base, -(exp + 1)));
+      } else {
+        return base * exponent(base, (exp - 1));
+      }
+  }
 };
 
 // 8. Determine if a number is a power of two.
@@ -47,14 +122,35 @@ var exponent = function(base, exp) {
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
 var powerOfTwo = function(n) {
+  if (n < 1) {
+    return false;
+  } else if (n === 1) {
+    return true;
+  } else {
+    return powerOfTwo(n / 2);
+  }
 };
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+  if (string.length === 0) {
+    return '';
+  } else {
+    return reverse(string.substring(1)) + string.charAt(0);
+  }
 };
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  string = string.toLowerCase();
+
+  if (string === '') {
+    return true;
+  } else if (string[0] === string[string.length - 1]) {
+    return palindrome(string.slice(1, (string.length - 1)))
+  } else {
+    return false;
+  }
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
@@ -63,6 +159,19 @@ var palindrome = function(string) {
 // modulo(17,5) // 2
 // modulo(22,6) // 4
 var modulo = function(x, y) {
+  if (y === 0) {
+    return NaN;
+  }
+  if (x < 0) {
+    return -modulo(-x, y);
+  }
+  if (y < 0) {
+    return modulo(x, -y);
+  }
+  if (x < y) {
+    return x;
+  }
+  return modulo((x - y), y);
 };
 
 // 12. Write a function that multiplies two numbers without using the * operator or
